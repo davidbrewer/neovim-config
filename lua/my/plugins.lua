f222af45431e869ddcfa6c85d11d82d307d61574
David Brewer <david.brewer@gmail.com>
2024-09-15 12:24:03 -0700

    Switch to a different, non-abandoned typescript provider.

--- lua/my/plugins.lua
+++ lua/my/plugins.lua
@@ -1,34 +1,40 @@
 return require("packer").startup(function(use)
 	use("wbthomason/packer.nvim")
-	use("neovim/nvim-lspconfig")
-	use({ "ms-jpq/coq_nvim", run = "python3 -m coq deps" })
-	use({ "ms-jpq/coq.artifacts", branch = "artifacts" })
-	use({ "ms-jpq/coq.thirdparty", branch = "3p" })
-	use({
-		"jose-elias-alvarez/null-ls.nvim",
-		requires = { "nvim-lua/plenary.nvim" },
-	})
-	use("jose-elias-alvarez/typescript.nvim")
+    use("neovim/nvim-lspconfig")
+    use({ "ms-jpq/coq_nvim", run = "python3 -m coq deps" })
+    use({ "ms-jpq/coq.artifacts", branch = "artifacts" })
+    use({ "ms-jpq/coq.thirdparty", branch = "3p" })
+    use({
+        "jose-elias-alvarez/null-ls.nvim",
+        requires = { "nvim-lua/plenary.nvim" },
+    })
+    use {
+      "pmizio/typescript-tools.nvim",
+      requires = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+      config = function()
+        require("typescript-tools").setup {}
+      end,
+    }
 
 	-- use("wellle/targets.vim")
-	use("scrooloose/nerdcommenter")
-	use("overcache/NeoSolarized")
-	use("tpope/vim-repeat")
+    use("scrooloose/nerdcommenter")
+    use("overcache/NeoSolarized")
+    use("tpope/vim-repeat")
 	-- use("tpope/vim-commentary")
-	use("tpope/vim-fugitive")
-	use("tpope/vim-surround")
-	use({ "lukas-reineke/indent-blankline.nvim", tag = "v2.20.8" })
+    use("tpope/vim-fugitive")
+    use("tpope/vim-surround")
+    use({ "lukas-reineke/indent-blankline.nvim", tag = "v2.20.8" })
 	-- use("alvan/vim-closetag")
-	use("ethanholz/nvim-lastplace")
-	use("mcauley-penney/tidy.nvim")
-	use("vim-autoformat/vim-autoformat")
+    use("ethanholz/nvim-lastplace")
+    use("mcauley-penney/tidy.nvim")
+    use("vim-autoformat/vim-autoformat")
 
 	-- use("ggandor/leap.nvim")
 	-- use({ "j-hui/fidget.nvim", tag = "legacy" })
 
 	-- If having issues with folders, refer to: https://github.com/nvim-treesitter/nvim-treesitter/wiki/Installation
-	use({ "nvim-treesitter/nvim-treesitter", tag = "v0.9.2", run = ":TSUpdate" })
-	use("nvim-treesitter/nvim-treesitter-textobjects")
+    use({ "nvim-treesitter/nvim-treesitter", tag = "v0.9.2", run = ":TSUpdate" })
+    use("nvim-treesitter/nvim-treesitter-textobjects")
 
 	-- Requires local dependencies: ripgrep (rg) and sed
 	-- Install those using homebrew: `brew install ripgrep gnu-sed`
@@ -39,30 +45,30 @@ return require("packer").startup(function(use)
 
 	-- Requires local dependencies: ripgrep (rg) and fd
 	-- Install those using homebrew: `brew install ripgrep fd`
-	use({
-		"nvim-telescope/telescope.nvim",
-		branch = "0.1.x",
-		requires = { { "nvim-lua/plenary.nvim" } },
-	})
+    use({
+        "nvim-telescope/telescope.nvim",
+        branch = "0.1.x",
+        requires = { { "nvim-lua/plenary.nvim" } },
+    })
 
-	use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" })
+    use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" })
 
-	use({
-		"nvim-tree/nvim-tree.lua",
-		requires = { "nvim-tree/nvim-web-devicons" },
-		tag = "v1.6.1",
-	})
+    use({
+        "nvim-tree/nvim-tree.lua",
+        requires = { "nvim-tree/nvim-web-devicons" },
+        tag = "v1.6.1",
+    })
 
-	use({ "lewis6991/gitsigns.nvim" })
+    use({ "lewis6991/gitsigns.nvim" })
 
-	use({
-		"nvim-lualine/lualine.nvim",
-		requires = { "kyazdani42/nvim-web-devicons", opt = true },
-	})
+    use({
+        "nvim-lualine/lualine.nvim",
+        requires = { "kyazdani42/nvim-web-devicons", opt = true },
+    })
 
-	use({
-		"akinsho/bufferline.nvim",
-		tag = "v3.*",
-		requires = "nvim-tree/nvim-web-devicons",
-	})
+    use({
+        "akinsho/bufferline.nvim",
+        tag = "v4.*",
+        requires = "nvim-tree/nvim-web-devicons",
+    })
 end)
